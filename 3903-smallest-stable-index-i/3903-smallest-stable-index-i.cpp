8033024da862ffee40300c899629a13454da589f
Affan Malik <affan.25005019@kiet.edu>
2026-09-04 23:11:56 +0530

    Time: 0 ms (100%), Space: 30.7 MB (73.61%) - LeetHub

---
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        int suf[100];
+        int n = nums.size();
+        suf[n-1] = nums.back();
+        for(int i=n-2; i>=0; i--)
+            suf[i] = min(suf[i+1], nums[i]);
+        
+        int maxsofar = 0;
+        for(int i=0; i<n; i++){
+            maxsofar = max(maxsofar, nums[i]);
+            if(maxsofar - suf[i] <= k)
+                return i;
+        }
+        return -1;
+    }
+};
